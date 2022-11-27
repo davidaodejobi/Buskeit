93c23faab9c98ffce8ff22fdacc325fa81cbe0ef
davidaodejobi <talk2engineerdavid@gmail.com>
2022-11-27 05:29:47 +0100

    fix: :lipstick: chaged the verfiy screen to a bottom sheet

--- lib/modules/auth_flow/screens/sign_up.dart
+++ lib/modules/auth_flow/screens/sign_up.dart
@@ -1,6 +1,7 @@
-import 'package:buskeit/modules/auth_flow/screens/verify_email.dart';
+import 'package:buskeit/modules/auth_flow/screens/user_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../constant/constant.dart';
@@ -74,7 +75,7 @@ class SignUp extends StatelessWidget {
                   ),
                   const XMargin(10),
                   Text(
-                    'Remind me',
+                    'Remember me',
                     style: Theme.of(context).textTheme.headline6!.copyWith(
                           fontWeight: FontWeight.w500,
                         ),
@@ -91,13 +92,14 @@ class SignUp extends StatelessWidget {
                     // if (provider.validatePassword(context)) {
                     //   provider.signup(context: context);
                     // }
-                    provider.signupMock();
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const VerifyEmail(
-                                  email: 'hello',
-                                )));
+                    verificationModal(context);
+                    // provider.signupMock();
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (context) => const VerifyEmail(
+                    //               email: 'hello',
+                    //             )));
                   },
                   child: provider.isLoading
                       ? const SpinKitChasingDots(
@@ -147,6 +149,155 @@ class SignUp extends StatelessWidget {
         horizontal: 16,
         vertical: 16,
       ),
+    );
+  }
+}
+
+Future<dynamic> verificationModal(BuildContext context) {
+  return showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      context: context,
+      enableDrag: false,
+      isScrollControlled: true,
+      //TODO: change to false
+      // isDismissible: false,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 16,
+            left: 16,
+            right: 16,
+          ),
+          child: const BottomVerify(),
+        );
+      });
+}
+
+class BottomVerify extends StatefulWidget {
+  const BottomVerify({super.key});
+
+  @override
+  State<BottomVerify> createState() => _BottomVerifyState();
+}
+
+class _BottomVerifyState extends State<BottomVerify> {
+  late TextEditingController _pinController;
+
+  @override
+  void initState() {
+    _pinController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pinController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<SignupProvider>(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text(
+          'Verfiy your account',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        const YMargin(40),
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'A verification code was sent to\n',
+                style: Theme.of(context).textTheme.headline6!.copyWith(
+                      color: AppColor.greyColor,
+                    ),
+              ),
+              TextSpan(
+                text: 'widget.email',
+                style: Theme.of(context).textTheme.headline6!.copyWith(
+                      color: AppColor.greyColor2,
+                    ),
+              ),
+            ],
+          ),
+        ),
+        const YMargin(30),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: PinCodeTextField(
+            length: 4,
+            keyboardType: TextInputType.number,
+            obscureText: false,
+            animationType: AnimationType.fade,
+            pinTheme: PinTheme(
+              activeColor: AppColor.tertiaryColor,
+              inactiveColor: AppColor.greyColor,
+              selectedFillColor: AppColor.primaryColor.withOpacity(0.1),
+              selectedColor: AppColor.primaryColor.withOpacity(0.5),
+              inactiveFillColor: Colors.transparent,
+              shape: PinCodeFieldShape.box,
+              borderRadius: BorderRadius.circular(5),
+              fieldHeight: 50,
+              fieldWidth: 50,
+              activeFillColor: Colors.white,
+            ),
+            animationDuration: const Duration(milliseconds: 300),
+            enableActiveFill: true,
+            hapticFeedbackTypes: HapticFeedbackTypes.vibrate,
+            useHapticFeedback: true,
+            enablePinAutofill: true,
+            controller: _pinController,
+            // onCompleted: (v) {
+            //   provider.verify(context: context);
+            // },
+            onChanged: (value) {},
+            beforeTextPaste: (text) {
+              //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+              //but you can show anything you want here, like your pop up saying wrong paste format or etc
+              return true;
+            },
+            appContext: context,
+          ),
+        ),
+        const YMargin(50),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              // provider.verify(context: context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UserDetails(),
+                ),
+              );
+            },
+            child: provider.isLoading
+                ? const SpinKitChasingDots(
+                    color: Colors.white,
+                    size: 30,
+                  )
+                : Text(
+                    'Verify',
+                    style: Theme.of(context).textTheme.headline5!.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+          ),
+        ),
+        const YMargin(20),
+      ],
     );
   }
 }
